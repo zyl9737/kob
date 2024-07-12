336be41cbcea3fd0d6c89b87fa8279351e500ed9
zyl9737 <973764862@qq.com>
2024-07-12 16:31:24 +0800

    实现登录和注册

--- backend/src/main/java/com/kob/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/kob/backend/config/SecurityConfig.java
@@ -1,17 +1,51 @@
 package com.kob.backend.config;
 
+import com.kob.backend.config.filter.JwtAuthenticationTokenFilter;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
 
+    @Autowired
+    private JwtAuthenticationTokenFilter jwtAuthenticationTokenFilter;
+
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManagerBean(
+            AuthenticationConfiguration authenticationConfiguration
+    ) throws Exception {
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf().disable()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/user/account/login/", "/user/account/register/").permitAll() //公开
+                .antMatchers(HttpMethod.OPTIONS).permitAll()
+                .anyRequest().authenticated();
+
+        http.addFilterBefore(jwtAuthenticationTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
+        return http.build();
     }
 }
--- web/src/router/index.js
+++ web/src/router/index.js
@@ -1,51 +1,97 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import PKIndexView from '../views/pk/PKIndexView.vue'
-import RanklistIndexView from '../views/ranklist/RanklistIndexView.vue'
-import RecordIndexView from '../views/record/RecordIndexView.vue'
-import UserBotIndexView from '../views/user/bots/UserBotIndexView.vue'
-import NotFound from '../views/error/NotFound.vue'
-
+import { createRouter, createWebHistory } from "vue-router";
+import PKIndexView from "@/views/pk/PKIndexView";
+import RecordIndexView from "@/views/record/RecordIndexView";
+import RanklistIndexView from "@/views/ranklist/RanklistIndexView";
+import MyBotsIndexView from "@/views/user/mybots/MyBotsIndexView";
+import NotFoundView from "@/views/errors/NotFoundView";
+import UserAccountLoginView from "@/views/user/account/UserAccountLoginView";
+import UserAccountRegisterView from "@/views/user/account/UserAccountRegisterView";
+import store from "@/store/index";
 
 const routes = [
   {
-    path: '/',
-    name: 'home',
-    redirect: '/pk/',
+    path: "/",
+    name: "home",
+    redirect: "/pk/",  // 如果是根路径则重定向到对战页面
+    meta: {
+      requestAuth: true,
+    },
   },
   {
-    path: '/pk/',
-    name: 'pk_index',
+    path: "/pk/",
+    name: "pk_index",
     component: PKIndexView,
+    meta: {
+      requestAuth: true,
+    },
   },
   {
-    path: '/record/',
-    name: 'record_index',
+    path: "/record/",
+    name: "record_index",
     component: RecordIndexView,
+    meta: {
+      requestAuth: true,
+    },
   },
   {
-    path: '/ranklist/',
-    name: 'ranklist_index',
+    path: "/ranklist/",
+    name: "ranklist_index",
     component: RanklistIndexView,
+    meta: {
+      requestAuth: true,
+    },
   },
   {
-    path: '/user/bot/',
-    name: 'user_bot_index',
-    component: UserBotIndexView,
+    path: "/user/mybots/",
+    name: "user_mybots_index",
+    component: MyBotsIndexView,
+    meta: {
+      requestAuth: true,
+    },
   },
   {
-    path: '/404/',
-    name: '404',
-    component: NotFound,
+    path: "/user/account/login/",
+    name: "user_account_login",
+    component: UserAccountLoginView,
+    meta: {
+      requestAuth: false,
+    },
   },
   {
-    path: '/:catchAll(.*)',
-    redirect: '/404/',
+    path: "/user/account/register/",
+    name: "user_account_register",
+    component: UserAccountRegisterView,
+    meta: {
+      requestAuth: false,
+    },
   },
-]
+  {
+    path: "/404/",
+    name: "404",
+    component: NotFoundView,
+    meta: {
+      requestAuth: false,
+    },
+  },
+  {
+    path: "/:catchAll(.*)",
+    name: "others",
+    redirect: "/404/",  // 如果不是以上路径之一说明不合法，重定向到404页面
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(),
-  routes
-})
+  routes,
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (to.meta.requestAuth && !store.state.user.is_login) {
+    alert("Please login!");
+    next({ name: "user_account_login" });
+  } else {
+    next();  // 如果不需要授权就直接跳转即可
+  }
+});
+
+export default router;
