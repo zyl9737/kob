fc0841f2e40fb82ac987b95c475da6805b37cfe9
zyl9737 <973764862@qq.com>
2024-07-16 10:08:22 +0800

    5.创建个人中心页面（上）

---
+++ backend/src/main/java/com/kob/backend/service/impl/user/bot/RemoveServiceImpl.java
@@ -0,0 +1,49 @@
+package com.kob.backend.service.impl.user.bot;
+
+import com.kob.backend.mapper.BotMapper;
+import com.kob.backend.pojo.Bot;
+import com.kob.backend.pojo.User;
+import com.kob.backend.service.impl.utils.UserDetailsImpl;
+import com.kob.backend.service.user.bot.RemoveService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Service
+public class RemoveServiceImpl implements RemoveService {
+    @Autowired
+    private BotMapper botMapper;
+
+    @Override
+    public Map<String, String> remove(Map<String, String> data) {
+        UsernamePasswordAuthenticationToken authenticationToken =
+                (UsernamePasswordAuthenticationToken) SecurityContextHolder.getContext().getAuthentication();
+        UserDetailsImpl loginUser = (UserDetailsImpl) authenticationToken.getPrincipal();
+        User user = loginUser.getUser(); // 获取当前登录用户
+
+        int bot_id = Integer.parseInt(data.get("bot_id"));
+        Bot bot = botMapper.selectById(bot_id);
+        Map<String, String> map = new HashMap<>();
+
+        if (bot == null) {
+            map.put("result", "The bot does not exist or has been deleted!");
+            return map;
+        }
+
+        if (!bot.getUserId().equals(user.getId())){
+            map.put("result", "You do not have permission to delete this bot!");
+            return map;
+        }
+        botMapper.deleteById(bot_id);
+
+        map.put("result", "success");
+
+        return map;
+    }
+
+
+}
